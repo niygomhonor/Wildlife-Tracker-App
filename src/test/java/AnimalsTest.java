34ef1086e95db5b85e1984481ffd77c71f31cbe6
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-09-19 17:41:07 +0200

    Add file of rangers

--- src/test/java/AnimalsTest.java
+++ src/test/java/AnimalsTest.java
@@ -2,20 +2,22 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class AnimalsTest{
-@Test
-public void Animals_instantiatesCorrectly_true() {
-    Animals animalTest = new Animals("Dog",1);
-    assertEquals(true, animalTest instanceof Animals);
+public class AnimalsTest {
+    @Test
+    public void Animals_instantiatesCorrectly_true() {
+        Animals animalTest = new Animals("Dog", 1);
+        assertEquals(true, animalTest instanceof Animals);
 
+    }
+
+    public void Animals_instantiating_WithName_String() {
+        Animals animalTest = new Animals("Dog", 1);
+        assertEquals("Dog", animalTest.getName());
+    }
+
+    @Test
+    public void Animals_instantiatesWithAnimalId_int() {
+        Animals animalTest = new Animals("Dog", 1);
+        assertEquals(1, animalTest.getAnimalId());
+    }
 }
-public void Animals_instantiating_WithName_String(){
-   Animals animalTest = new Animals("Dog",1);
-      assertEquals("Dog", animalTest.getName());
-}
-         @Test
-      public void Animals_instantiatesWithAnimalId_int() {
-            Animals animalTest=new Animals("Dog",1)  ;
-             assertEquals(1,animalTest.getAnimalId());
-         }
-         }
--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -1,3 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class EndangeredAnimalTest {
+    @Test
+
+    public void EndangeredAnimal_Instantiating_correctly() {
+
+        EndangeredAnimal endangeredAnimal = new EndangeredAnimal("Elephant");
+        assertEquals(true, endangeredAnimal instanceof EndangeredAnimal);
+    }
 
 }
