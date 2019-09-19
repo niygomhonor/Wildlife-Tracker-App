a0a9bb09c486b570a5ee5984ceb0e75ffbf99b24
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-09-19 15:53:33 +0200

    Add test files and codes to test class

--- src/test/java/AnimalsTest.java
+++ src/test/java/AnimalsTest.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import org.junit.Test;
 
-public class AnimalsTest {
+import static org.junit.Assert.assertEquals;
+
+public class AnimalsTest{
+@Test
+public void Animals_instantiatesCorrectly_true() {
+    Animals animalTest = new Animals("Dog",1);
+    assertEquals(true, animalTest instanceof Animals);
+
 }
+public void Animals_instantiating_WithName_String(){
+   Animals animalTest = new Animals("Dog",1);
+      assertEquals("Dog", animalTest.getName());
+}
+         @Test
+      public void Animals_instantiatesWithAnimalId_int() {
+            Animals animalTest=new Animals("Dog",1)  ;
+             assertEquals(1,animalTest.getAnimalId());
+         }
+         }
